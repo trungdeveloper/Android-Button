d870e62a0ed673469be7ff209d7f6abc821988be
Nguyen Tan Trung <trung.nguyen@pn.com.vn>
2019-09-16 08:23:33 +0700

    bla

---
+++ app/src/main/java/com/example/androidbutton/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setText("submit 2");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
